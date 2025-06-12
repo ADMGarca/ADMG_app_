41721c4d0d5997c8e9f4840611cdee6bb9dd29fc
WLPereira <washingtton_@hotmail.com>
2025-06-12 00:59:08 -0300

    build:Atualizar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7ffd8e026001d16c934830ececa73493
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "51ae7d3ca139bfb36c1f72e5bf6c34c8",
+"flutter_bootstrap.js": "dfe580b2c1361832c84a586793b7cf80",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
